7b1cc1852cf6eeaf4417a1d15079dab211031931
Vjera Bulatovic <vb6710@rit.edu>
2025-09-17 23:05:30 +0200

    commiting

--- characters.py
+++ characters.py
@@ -5,3 +5,27 @@
 @ASSESSME.ANALYZE: YES
 '''
 
+def add_chars(char1, char2):
+    character1 = ord(char1)
+    character2 = ord(char2)
+    sum = character1 + character2
+    sum_char = chr(sum)
+    print("Sum of a and b =" , sum,sep=" ",end="\n")
+    print("Character =", sum_char ,sep=" ")   
+ 
+ 
+ 
+def substract_chars(char3,char4):
+    character3 = ord(char3)
+    character4 = ord(char4)
+    substract = character3 - character4 
+    characters=chr(substract)
+    print("Difference of c and d =", substract, sep=" ", end="\n")
+    print("character=", characters, sep=" ")
+
+def main():
+    add_chars("a","b")
+    substract_chars("c","a")
+
+if __name__ == "__main__":
+    main()
--- scope.py
+++ scope.py
@@ -1,6 +1,33 @@
 '''
-@ASSESSME.USERID: userID
+@ASSESSME.USERID: vb6710
 @ASSESSME.AUTHOR: author, list of authors
 @ASSESSME.DESCRIPTION: Assignment 2.2
 @ASSESSME.ANALYZE: YES
 '''
+
+STRING_GLOBAL = "Hellooooo guyssssss!!!!"
+INT_GLOBAL = 12345
+FLOAT_GLOBAL = 3.14159
+
+
+def print_param(para):
+    print("Parameter value :", para, sep=" ")
+
+def print_local():
+    local = "local variable"
+    print("Local variable:", local, sep=" ")
+
+def print_which():
+    print("Global string: ", STRING_GLOBAL, sep=" ")
+
+def main():
+    print_param(STRING_GLOBAL)
+    print_param(INT_GLOBAL)
+    print_param(FLOAT_GLOBAL)
+
+    print_local()
+    print_which()
+    print("Global variable:", STRING_GLOBAL)
+
+if __name__ == "__main__":
+    main()
